756628d0079f02ce17d717de053592c38dc50737
chongo-Huck <noaj212@gmail.com>
2024-06-17 18:25:44 -0500

    Update scripts.js
    Added Pokemon for exercise 1.2

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood='Green Curry';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Starmie', height: 30, types: ['grass', 'poison']},
+    {name: 'Mew', height: 50, types: ['psychic', 'water']},
+    {name: 'Onyx', height: 100, types: ['rock', 'ground']},
+]
